5c776e30c3a2df28cd7d132bdb9c536d1cfe1696
aliagagf <guilhermealiaga@usp.br>
2024-03-07 14:20:23 -0300

    melhorado

--- firstChallenge.js
+++ firstChallenge.js
@@ -5,20 +5,19 @@ let stringTrinta = '30'
 let numeroDez = 10
 let stringDez = '10'
 
-if (numeroUm == stringUm && !(numeroUm === stringUm)) {
-  console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes')
-} else if (!numeroUm == stringUm) {
-  console.log('As variáveis numeroUm e stringUm não tem o mesmo valor')
+function compare (numero, string){
+  if(numero == string){
+    if (typeof numero === typeof string)
+      console.log('As variáveis tem o mesmo valor e mesmo tipo');
+    else
+      console.log('As variáveis tem o mesmo valor e tipos diferentes');
+  }
+  else{
+    if (typeof numero === typeof string)
+      console.log('As variáveis tem valores diferentes e mesmo tipo');
+    else
+      console.log('As variáveis tem valores diferentes e tipos diferentes');
+  }
 }
 
-if (numeroTrinta === stringTrinta) {
-  console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo')
-} else {
-  console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo')
-}
-
-if (numeroDez == stringDez && !(numeroDez === stringDez)) {
-  console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes')
-} else {
-  console.log('As variáveis numeroDez e stringDez não tem o mesmo valor')
-}
+compare (numeroUm, stringUm)
